f479388149121cd33b7c5a8d6d52587020d18c6d
HongTak <qoqo110966@gmail.com>
2025-02-21 02:07:37 +0900

    kaplayCtx
    kaplayCtx 속성값 추가

--- src/ReactComponents/kaplayCtx.js
+++ src/ReactComponents/kaplayCtx.js
@@ -5,5 +5,10 @@ export default function initKaplay(ctx) {
     width : 1920,
     height : 1080,
     letterbox : true,
+    global : false,
+    debug : true, // TODO: put back to false in prod
+    debugKey : "f1",
+    canvas : document.getElementById("game"),
+    pixelDensity: devicePixelRatio,
   });
 }
